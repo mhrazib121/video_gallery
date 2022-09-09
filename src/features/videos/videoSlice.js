0a2b603ddc95508b21a333fc8eecb6c2282d92e3
mhrazib121 <mhrazib121@gmail.com>
2022-09-09 09:22:55 +0600

    first commit

---
+++ src/features/videos/videoSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getVideos } from "./videosAPI";
+
+const initialState ={
+    videos:[],
+    isLoading: false,
+    isError: false,
+    error:'',    
+};
+
+// create thunk function 
+export const fetchVideos = createAsyncThunk("videos/fetchVideos", async ()=>{
+    const videos = await getVideos();
+    return videos;
+})
+
+const videoSlice = createSlice({
+    name: 'videos',
+    initialState,
+    extraReducers: (builder) =>{
+        builder
+        .addCase(fetchVideos.pending, (state) => {
+            state.isLoading= true;
+            state.isError= false;
+            state.videos= [];
+        })
+        .addCase(fetchVideos.fulfilled, (state, action) => {
+            state.isLoading= false;
+            state.isError= false;
+            state.videos= action.payload;
+        })
+        .addCase(fetchVideos.rejected, (state, action) => {
+            state.isLoading= false;
+            state.isError= true;
+            state.error= action.payload?.message;
+            state.videos= [];
+        })
+    }
+});
+
+export default videoSlice.reducer;
---
+++ src/features/videos/videosAPI.js
@@ -0,0 +1,8 @@
+import axiosInstance from "../../utils/axios";
+
+export const getVideos = async () =>{
+    const response = await axiosInstance.get("/videos");
+
+    return response.data;
+
+};
